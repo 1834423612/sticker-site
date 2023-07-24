82417aed6c5119ce106854fae2cf43215734a53f
Mingri159 <1593683331@qq.com>
2023-07-24 15:58:29 +0800

    add: element-plus-icon

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -24,5 +25,8 @@ app.use(Pinia)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 app.mount('#app')
